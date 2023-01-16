23cfa66c5a9bba9fef072f2ed3a2d9b7b0f1ac2c
Saeed Ahmadvand <ahmadvand.saeed@yahoo.com>
2023-01-16 15:28:59 +0330

    change AggregateRoot and Entity base class

--- Shoppy.Domain/Entity.cs
+++ Shoppy.Domain/Entity.cs
@@ -5,7 +5,7 @@ namespace Shoppy.Domain
     public abstract class Entity<TKey> : IEntity<TKey>
         where TKey : IEquatable<TKey>
     {
-        public TKey Id { get; protected set; } = default!;
+        public virtual TKey Id { get; protected set; } = default!;
 
         public override bool Equals(object? obj)
         {
@@ -13,13 +13,28 @@ namespace Shoppy.Domain
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != GetType()) return false;
             if (obj is not Entity<TKey> entity) return false;
-
+            if (GetUnproxiedType(this) != GetUnproxiedType(obj)) return false;
+            if (Id.Equals(default) || entity.Id.Equals(default))return false;
             return Id.Equals(entity.Id);
+        }
+
+        public Type GetUnproxiedType(object obj)
+        {
+            const string EFCoreProxyPrefix = "Castle.Proxies.";
+            const string NHibernateProxyPostfix = "Proxy";
+
+            Type type = obj.GetType();
+            string typeString = type.ToString();
+
+            if (typeString.Contains(EFCoreProxyPrefix) || typeString.EndsWith(NHibernateProxyPostfix))
+                return type.BaseType;
+
+            return type;
         }
 
         public override int GetHashCode()
         {
-            return Id.GetHashCode();
+            return (GetUnproxiedType(this).ToString() + Id).GetHashCode();
         }
     }
 }
